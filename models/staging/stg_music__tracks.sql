90ddedadce1460c7841ae38a3f99ac93a74f2065
Aman Dutta <amandutta004@gmail.com>
2024-04-29 06:21:59 +0000

    Updated the names to lowercase

--- models/staging/stg_music__tracks.sql
+++ models/staging/stg_music__tracks.sql
@@ -1,13 +1,13 @@
 select 
-    TrackId as track_id,
-    Name as track_name,
-    MediaTypeId as mediatype_id,
-    GenreId as genre_id,
-    Composer as track_composer,
-    Milliseconds as track_length_ms,
-    Bytes as bytes,
-    UnitPrice as unit_price,
-    Milliseconds/1000 as track_length_seconds,
-    Milliseconds/60000 as track_length_mins
+    trackid as track_id,
+    name as track_name,
+    mediatypeid as mediatype_id,
+    genreid as genre_id,
+    composer as track_composer,
+    milliseconds as track_length_ms,
+    bytes as bytes,
+    unitprice as unit_price,
+    milliseconds/1000 as track_length_seconds,
+    milliseconds/60000 as track_length_mins
 from {{ source('music', 'track') }}
 
